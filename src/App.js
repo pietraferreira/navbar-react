18cd78db9e03535304f59c1a77b4f81b3d133d9f
Pietra Ferreira <piferreira@icloud.com>
2022-10-18 20:50:01 +0100

    navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Navbar from './Navbar';
 import './App.css';
+import { Component } from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <Navbar />
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
+  </>
   );
 }
 
